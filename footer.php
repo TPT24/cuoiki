44798d83b2629385b05ca494940f7a3aec214fb7
TPT24 <tpthinha12k4@gmail.com>
2026-01-05 22:07:52 +0700

    Add new files

--- footer.php
+++ footer.php
@@ -1,2 +1,5 @@
 <hr>
 <div>Truyền thống 25 năm Ngành CNTT, Trường Đại học Vinh (1998-2023)</div>
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -1,6 +1,12 @@
 <!doctype html>
-<meta charset="utf-8">
-<link rel="stylesheet" href="style.css">
-<div>Hôm nay: <?= date("d/m/Y") ?> | Giờ: <?= date("H:i:s") ?></div>
-<div>Phát triển ứng dụng với Mã nguồn mở</div>
-<hr>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div>Hôm nay: <?= date("d/m/Y") ?> | Giờ: <?= date("H:i:s") ?></div>
+    <div>Phát triển ứng dụng với Mã nguồn mở</div>
+    <hr>
